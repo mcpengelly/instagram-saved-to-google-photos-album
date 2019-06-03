204772e9f1e14dd139b4067bf04d7a05ff785b09
mcpengelly <pengelly.mat@gmail.com>
2019-06-03 15:04:02 -0400

    init

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
-  username: '',
-  password: '',
+  username: "junk",
+  password: "junk"
 };
--- index.js
+++ index.js
@@ -1,102 +1,106 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const fs = require("fs");
-const Promise = require("bluebird");
-const SavedPost = require("./saved-post");
-const Credentials = require("./credentials");
+// Nodejs
+// const fs = require("fs");
+// const { exec } = require("child_process");
 
-const Client = require("instagram-private-api").V1;
+// // Third party
+// const request = require("request");
+// const Client = require("instagram-private-api").V1;
 
-const username = Credentials.username;
-const password = Credentials.password;
+// Local
+// const { username, password } = require("./credentials");
 
-console.log(username);
-console.log(password);
+// const result = await request.get(config.apiEndpoint + '/v1/albums', {
+//   headers: {'Content-Type': 'application/json'},
+//   qs: parameters,
+//   json: true,
+//   auth: {'bearer': authToken},
+// });
 
-const device = new Client.Device(username);
-const storage = new Client.CookieMemoryStorage();
-
-const flatten = ary => {
-  let ret = [];
-  for (let i = 0; i < ary.length; i += 1) {
-    if (Array.isArray(ary[i])) {
-      ret = ret.concat(flatten(ary[i]));
-    } else {
-      ret.push(ary[i]);
-    }
-  }
-  return ret;
-};
-
-Client.Session.create(device, storage, username, password)
-  .then(session => {
-    return new Client.Feed.SavedMedia(session, 100).all();
-  })
-  .then(posts => {
-    const savedPosts = posts.map(post => {
-      const { webLink, caption } = post.params;
-
-      const findImagesUrl = obj => {
-        if (Array.isArray(obj)) {
-          return obj.map(o => {
-            return findImagesUrl(o);
-          });
-        }
-        return obj.url;
-      };
-
-      let imagesLink = flatten(findImagesUrl(post.params.images));
-
-      imagesLink = imagesLink.filter((obj, index) => {
-        return index % 2 === 0;
-      });
-
-      var tokens = [];
-
-      if (caption) {
-        tokens = caption.split(" ");
-      }
-
-      let hashtags = [];
-      let words = [];
-      let mentions = [];
-      for (token of tokens) {
-        if (token[0] == "#") {
-          hashtags.push(token);
-        } else if (token[0] == "@") {
-          mentions.push(token);
-        } else {
-          words.push(token);
-        }
-      }
-
-      return new SavedPost(
-        webLink,
-        caption,
-        imagesLink,
-        hashtags.join(" "),
-        words.join(" "),
-        mentions.join(" ")
-      );
-    });
-
-    return new Promise((resolve, reject) => {
-      const path = `${__dirname}/saved_posts.json`;
-
-      const content = JSON.stringify(savedPosts, null, 4);
-
-      fs.writeFile(path, content, "utf8", err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(path);
-        }
-      });
-    });
-  })
-  .then(path => {
-    console.log(`File saved at ${path}`);
-  })
-  .catch(err => {
-    console.log(err);
+const {auth} = require('google-auth-library');
+ 
+/**
+ * Instead of specifying the type of client you'd like to use (JWT, OAuth2, etc)
+ * this library will automatically choose the right client based on the environment.
+ */
+async function main() {
+  const client = await auth.getClient({
+    scopes: 'https://www.googleapis.com/auth/cloud-platform'
   });
+  const projectId = await auth.getProjectId();
+  const url = `https://www.googleapis.com/dns/v1/projects/${projectId}`;
+  const res = await client.request({ url });
+  console.log(res.data);
+}
+ 
+main().catch(console.error);
+
+
+// const device = new Client.Device(username);
+// const storage = new Client.CookieMemoryStorage();
+
+// const removeWhitespace = str => str.replace(/\r?\n|\r/g, "");
+
+// const getSavedPosts = session => new Client.Feed.SavedMedia(session, 100).all();
+
+// const getImages = posts =>
+//   posts.reduce((accum, current) => {
+//     const { caption, images, user } = current.params;
+//     const image = Array.isArray(images) ? images[0] : null;
+
+//     // if no image data is found then ignore this post
+//     if (!image) {
+//       return accum;
+//     }
+
+//     const postCaption = removeWhitespace(caption.text);
+//     accum.push({ username: user.username, caption: postCaption, image });
+//     return accum;
+//   }, []);
+
+// const saveToFile = data =>
+//   new Promise((resolve, reject) => {
+//     const path = `${__dirname}/saved_posts.json`;
+//     const content = JSON.stringify(data, null, 4);
+
+//     fs.writeFile(path, content, "utf8", err => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data, path);
+//       }
+//     });
+//   });
+
+// const saveImagesToDisk = data => {
+//   new Promise((resolve, reject) => {
+//     let count = 0;
+//     data.forEach(post => {
+//       const download = (uri, filename, callback) => {
+//         request.head(uri, (err, res, body) => {
+//           request(uri)
+//             .pipe(fs.createWriteStream(filename))
+//             .on("close", callback);
+//         });
+//       };
+//       const filename = "filename_" + count++ + ".jpg";
+//       download(post.image.url, filename, function() {
+//         console.log(filename, "done");
+//       });
+//     });
+//     resolve();
+//   });
+// };
+
+// Client.Session.create(device, storage, username, password)
+//   .then(getSavedPosts)
+//   .then(getImages)
+//   .then(saveToFile)
+//   .then(saveImagesToDisk)
+//   .then(() => {
+//     console.log(`File saved`);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
