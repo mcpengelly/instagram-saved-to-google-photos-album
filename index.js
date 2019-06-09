5763ba0632200186db7b208ded873f4c296f9b3f
mcpengelly <pengelly.mat@gmail.com>
2019-06-09 19:17:55 -0400

    redo boilerplate

--- index.js
+++ index.js
@@ -1,106 +1,33 @@
-// require("dotenv").config();
-
-// Nodejs
-// const fs = require("fs");
-// const { exec } = require("child_process");
-
-// // Third party
-// const request = require("request");
-// const Client = require("instagram-private-api").V1;
-
-// Local
-// const { username, password } = require("./credentials");
-
-// const result = await request.get(config.apiEndpoint + '/v1/albums', {
-//   headers: {'Content-Type': 'application/json'},
-//   qs: parameters,
-//   json: true,
-//   auth: {'bearer': authToken},
-// });
-
-const {auth} = require('google-auth-library');
- 
-/**
- * Instead of specifying the type of client you'd like to use (JWT, OAuth2, etc)
- * this library will automatically choose the right client based on the environment.
- */
-async function main() {
-  const client = await auth.getClient({
-    scopes: 'https://www.googleapis.com/auth/cloud-platform'
+import { IgApiClient } from "./src";
+import { sample } from "lodash";
+const ig = new IgApiClient();
+// You must generate device id's before login.
+// Id's generated based on seed
+// So if you pass the same value as first argument - the same id's are generated every time
+ig.state.generateDevice(process.env.IG_USERNAME);
+// Optionally you can setup proxy url
+ig.state.proxyUrl = process.env.IG_PROXY;
+(async () => {
+  // Execute all requests prior to authorization in the real Android application
+  // Not required but recommended
+  await ig.simulate.preLoginFlow();
+  const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  // The same as preLoginFlow()
+  // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
+  process.nextTick(async () => await ig.simulate.postLoginFlow());
+  // Create UserFeed instance to get loggedInUser's posts
+  const userFeed = ig.feed.user(loggedInUser.pk);
+  const myPostsFirstPage = await userFeed.items();
+  // All the feeds are auto-paginated, so you just need to call .items() sequentially to get next page
+  const myPostsSecondPage = await userFeed.items();
+  await ig.media.like({
+    // Like our first post from first page or first post from second page randomly
+    mediaId: sample([myPostsFirstPage[0].id, myPostsSecondPage[0].id]),
+    moduleInfo: {
+      module_name: "profile",
+      user_id: loggedInUser.pk,
+      username: loggedInUser.username
+    },
+    d: sample([0, 1])
   });
-  const projectId = await auth.getProjectId();
-  const url = `https://www.googleapis.com/dns/v1/projects/${projectId}`;
-  const res = await client.request({ url });
-  console.log(res.data);
-}
- 
-main().catch(console.error);
-
-
-// const device = new Client.Device(username);
-// const storage = new Client.CookieMemoryStorage();
-
-// const removeWhitespace = str => str.replace(/\r?\n|\r/g, "");
-
-// const getSavedPosts = session => new Client.Feed.SavedMedia(session, 100).all();
-
-// const getImages = posts =>
-//   posts.reduce((accum, current) => {
-//     const { caption, images, user } = current.params;
-//     const image = Array.isArray(images) ? images[0] : null;
-
-//     // if no image data is found then ignore this post
-//     if (!image) {
-//       return accum;
-//     }
-
-//     const postCaption = removeWhitespace(caption.text);
-//     accum.push({ username: user.username, caption: postCaption, image });
-//     return accum;
-//   }, []);
-
-// const saveToFile = data =>
-//   new Promise((resolve, reject) => {
-//     const path = `${__dirname}/saved_posts.json`;
-//     const content = JSON.stringify(data, null, 4);
-
-//     fs.writeFile(path, content, "utf8", err => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data, path);
-//       }
-//     });
-//   });
-
-// const saveImagesToDisk = data => {
-//   new Promise((resolve, reject) => {
-//     let count = 0;
-//     data.forEach(post => {
-//       const download = (uri, filename, callback) => {
-//         request.head(uri, (err, res, body) => {
-//           request(uri)
-//             .pipe(fs.createWriteStream(filename))
-//             .on("close", callback);
-//         });
-//       };
-//       const filename = "filename_" + count++ + ".jpg";
-//       download(post.image.url, filename, function() {
-//         console.log(filename, "done");
-//       });
-//     });
-//     resolve();
-//   });
-// };
-
-// Client.Session.create(device, storage, username, password)
-//   .then(getSavedPosts)
-//   .then(getImages)
-//   .then(saveToFile)
-//   .then(saveImagesToDisk)
-//   .then(() => {
-//     console.log(`File saved`);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+})();
